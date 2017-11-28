aefb4bc45c42c5e8b3f61239fe1fec7a9cc6e6be
WorldVirus <metallika956@gmail.com>
2017-11-28 06:28:20 +0300

    test

--- public/server.js
+++ public/server.js
@@ -1,93 +1,4 @@
-/*
-'use strict';
 
-const express = require('express');
-const body = require('body-parser');
-const cors = require('cors');
-const cookie = require('cookie-parser');
-const morgan = require('morgan');
-const uuid = require('uuid/v4');
-const app = express();
-
-
-app.use(cors({
-    origin: true,
-    credentials: true,
-}));
-
-app.use(morgan('dev'));
-app.use(express.static('public'));
-app.use(body.json());
-app.use(cookie());
-
-const users = {};
-const ids = {};
-
-app.post('/signup', function (req, res) {
-    const email = req.body.email;
-    const username = req.body.username;
-    const password = req.body.password;
-    console.log (req.body.username);
-    if (
-        !email || !username || !password ||
-        !email.match(/@/) ||
-        !username.match(/^\S{4,}$/) ||
-        !password.match(/^\S{4,}$/)
-
-    ) {
-        return res.status(400).json({error: 'not valid data'});
-    }
-    if (users[username]) {
-        return res.status(400).json({error: 'user is already exist'});
-    }
-
-    const id = uuid();
-    ids[id] = username;
-    users[username] = {password, email};
-
-
-    res.cookie('cookie', id, {expires: new Date(Date.now() + 1000 * 60 * 10)});
-    // res.json({id});
-});
-
-app.post('/signin', function (req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    if (!password || !username) {
-        return res.status(400).json({error: 'not valid e-mail or password'});
-    }
-    if (!users[username] || users[username].password !== password) {
-        return res.status(400).json({error: 'not valid e-mail or password'});
-    }
-
-    const id = uuid();
-    ids[id] = username;
-
-    res.cookie('cookie', id, {expires: new Date(Date.now() + 1000 * 60 * 10)});
-    // res.status(201).json({id});
-});
-
-app.post('/session', function (req, res) {
-    
-    res.status(200);
-});
-
-
-
-app.delete('/signout', function (req, res) {
-    res.cookie('cookie', null, {expires: new Date(Date.now() + 1000 * 60 * 10)});
-    res.status(200).json(null);
-});
-
-
-
-const port = process.env.PORT || 8080;
-
-app.listen(port, () => {
-    console.log(`Server listening port ${port}`);
-});
-*/
 'use strict';
 
 const fallback = require('express-history-api-fallback');
@@ -190,7 +101,7 @@ app.delete('/signout', function (req, res) {
 //     res.send('404');
 // });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
     console.log(`Server listening port ${port}`);
